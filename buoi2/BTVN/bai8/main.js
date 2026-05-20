b8aef7b9a0d4b74420209b60d9bcb85bdc03515d
unknown <email_cua_ban@example.com>
2026-05-20 16:36:39 +0700

    add bai8 buoi2

---
+++ buoi2/BTVN/bai8/main.js
@@ -0,0 +1,12 @@
+console.log(`in ra message: ${message}`);
+var message = "Hello, World!";
+
+{
+  var result = "Hello, World!";
+}
+console.log(`in ra result: ${result}`);
+
+{
+  let error = "Hello, World!";
+}
+console.log(`in ra error: ${error}`);
